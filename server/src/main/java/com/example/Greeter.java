8370fda22603593faae03c00b7de06bbd4d9692e
tessotyope <48215295+tessotyope@users.noreply.github.com>
2019-03-05 15:30:52 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
+  
+  /**
+  *@param someone of a person
+  *@return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
